1df1e2325b68ce3090879ff3b5214fd115ab521a
LAPTOP-2EM4M186\Naveen Selvaraj <naveenselvaraj93@gmail.com>
2020-07-18 20:07:29 +0530

    Added changes related to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t naveenselvaraj93/mult-client:latest -t naveenselvaraj93/multi-client:$SHA -f ./client/Dcokerfile ./client
-docker build -t naveenselvaraj93/mult-server:latest -t naveenselvaraj93/multi-server:$SHA -f ./server/Dcokerfile ./server
-docker build -t naveenselvaraj93/multi-worker:latest -t naveenselvaraj93/multi-worker:$SHA -f ./worker/Dcokerfile ./worker
+docker build -t naveenselvaraj/mult-client:latest -t naveenselvaraj/multi-client:$SHA -f ./client/Dcokerfile ./client
+docker build -t naveenselvaraj/mult-server:latest -t naveenselvaraj/multi-server:$SHA -f ./server/Dcokerfile ./server
+docker build -t naveenselvaraj/multi-worker:latest -t naveenselvaraj/multi-worker:$SHA -f ./worker/Dcokerfile ./worker
 
-docker push naveenselvaraj93/multi-client:latest
-docker push naveenselvaraj93/multi-worker:latest
-docker push naveenselvaraj93/multi-server:latest
+docker push naveenselvaraj/multi-client:latest
+docker push naveenselvaraj/multi-worker:latest
+docker push naveenselvaraj/multi-server:latest
 
-docker push naveenselvaraj93/multi-client:$SHA
-docker push naveenselvaraj93/multi-worker:$SHA
-docker push naveenselvaraj93/multi-server:$SHA
+docker push naveenselvaraj/multi-client:$SHA
+docker push naveenselvaraj/multi-worker:$SHA
+docker push naveenselvaraj/multi-server:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=naveenselvaraj93/multi-server:$SHA
-kubectl set image deployments/client-deployment client=naveenselvaraj93/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=naveenselvaraj93/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=naveenselvaraj/multi-server:$SHA
+kubectl set image deployments/client-deployment client=naveenselvaraj/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=naveenselvaraj/multi-worker:$SHA
